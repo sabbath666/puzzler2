830f48e5b4042922064d6987dc6378a5b22930c5
u001073 <_sabbath@mail.ru>
2018-12-28 05:43:37 +0300

    refactoring

--- src/main/kotlin/com/sabbath/puzzler/Utils.java
+++ src/main/kotlin/com/sabbath/puzzler/Utils.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Utils {
-    public static List<User> addNullUser() {
+    public static List<User> listOfNullUser() {
         return Collections.singletonList(null);
     }
 }
